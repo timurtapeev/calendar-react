205bdd9620d080ce5da73ee5538eee0dd5d5cad4
Tapeyev Timur <tspeevtimut@gmail.com>
2023-12-13 16:34:51 +0500

    Создана шапка. Создан компонент MainButton

---
+++ src/components/UI/MainButton/MainButton.jsx
@@ -0,0 +1,11 @@
+import classes from './MainButton.module.css'
+
+const MainButton = ({children, ...props}) => {
+    return (
+        <button {...props} className={classes.MainButton}>
+            { children }
+        </button>
+    )
+}
+
+export default MainButton
--- src/components/header/MyHeader.jsx
+++ src/components/header/MyHeader.jsx
@@ -1,9 +1,21 @@
-import MyButton from '../UI/button/MyButton'
+import MainButton from '../UI/MainButton/MainButton'
+import classes from './MyHeader.module.css'
 
 const MyHeader = () => {
     return (
-        <div>
-            <MyButton/>
+        <div className={classes.MyHeader}>
+            <div className="container">
+                <div className={classes.MyHeader__wrapper}>
+                    <div className={classes.MyHeader__buttons}>
+                        <MainButton> Добавить </MainButton>
+                        <MainButton disabled> Обновить </MainButton>
+                    </div>
+
+                    <div  className={classes.MyHeader__input}>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
