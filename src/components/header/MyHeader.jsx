5d42318170f562b0b0837a2dc9199fcc6bf278d9
Tapeyev Timur <tspeevtimut@gmail.com>
2023-12-13 17:23:16 +0500

    Создан и застилизован компонент MyInput

--- src/components/header/MyHeader.jsx
+++ src/components/header/MyHeader.jsx
@@ -1,18 +1,25 @@
 import MainButton from '../UI/MainButton/MainButton'
+import MyInput from '../UI/MyInput/MyInput'
 import classes from './MyHeader.module.css'
 
 const MyHeader = () => {
+
+
     return (
         <div className={classes.MyHeader}>
             <div className="container">
                 <div className={classes.MyHeader__wrapper}>
                     <div className={classes.MyHeader__buttons}>
-                        <MainButton> Добавить </MainButton>
+                        <MainButton disabled> Добавить </MainButton>
                         <MainButton disabled> Обновить </MainButton>
                     </div>
 
-                    <div  className={classes.MyHeader__input}>
-
+                    <div className={classes.MyHeader__input}>
+                        <MyInput
+                            type="text"
+                            invalid
+                            placeholder="Placeholder"
+                        />
                     </div>
                 </div>
             </div>
